964899c4ad745905e2fcdc2b091a1055b551b9ee
kamend0 <mendoza.kollin@gmail.com>
2022-06-07 21:50:30 -0700

    Add input check util function, API call skeleton to Homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
+import { removeSpaces } from './utils/inputChecks';
 
 // Child-Parent Communication SO:
 // https://stackoverflow.com/questions/35537229/how-can-i-update-the-parents-state-in-react
@@ -16,7 +17,38 @@ export default class Homepage extends Component {
         // Set state
         this.state = {
             query: new URLSearchParams(window.location.search).get('s'),
+            isLoaded: false,
+            resp: [],
+            userId: 0,
+            id: 0,
+            title: '',
+            completed: false,
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(result => result.json())
+            .then((data) => {
+                this.setState({
+                    isLoaded: true,
+                    resp: data,
+                    userId: data.userId,
+                    id: data.id,
+                    title: data.title,
+                    completed: data.completed,
+                });
+            },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+                  (error) => {
+                      this.setState({
+                          isLoaded: true,
+                          error,
+                      });
+                  },
+          );
     }
 
     render() {
@@ -24,15 +56,26 @@ export default class Homepage extends Component {
         // Then render based on presence of input
         // TODO: input checking should probably go here
         const currentQuery = this.state.query;
+        // const resultsLoaded = this.state.isLoaded;
         let queryInfo;
         if (currentQuery) {
             // User gave a query; check it, call API, etc.
             // When Results component exists, this could be: queryInfo = <Results />
             queryInfo = (
                 <div>
-                    <h2>You searched: {currentQuery}</h2>
+                    <h2>Cleaned search: {removeSpaces(currentQuery)}</h2>
+                    {/* <h2>Cleaned search: {currentQuery.trim()}</h2> */}
                     <h2>The type of the query: {typeof currentQuery}</h2>
                     <h2>Length of query provided: {currentQuery.length}</h2>
+                    <h2>Is loaded?: {String(this.state.isLoaded)}</h2>
+                    <h2>Results from call:</h2>
+                    <div>
+                        Data response: {String(this.state.resp)} <br />
+                        userId: {String(this.state.userId)} <br />
+                        id: {String(this.state.id)} <br />
+                        title: {String(this.state.title)} <br />
+                        completed: {String(this.state.completed)}
+                    </div>
                 </div>
             );
         } else {
---
+++ src/utils/inputChecks.js
@@ -0,0 +1,14 @@
+export function removeSpaces(inStr) {
+    /*
+    Remove any spaces after commas but before words, but not
+    the spaces within the search terms (e.g. "bell pepper").
+    Could probably do this way easier with Regex...
+    ...but learning Regex is the hard part
+    */
+    const splitStr = inStr.split(',');
+    let cleanedStr = splitStr[0].trim();
+    for (let i = 1; i < splitStr.length; i += 1) {
+        cleanedStr = `${cleanedStr},${splitStr[i].trim()}`;
+    }
+    return cleanedStr;
+}
