7ad2f56db4ab70143f8e7795d224b1af9bc2b4aa
kamend0 <mendoza.kollin@gmail.com>
2022-06-20 14:31:40 -0700

    Successfully render many results.

--- src/Homepage.js
+++ src/Homepage.js
@@ -18,33 +18,37 @@ export default class Homepage extends Component {
         this.state = {
             query: new URLSearchParams(window.location.search).get('s'),
             isLoaded: false,
-            resp: '',
-            title: '',
-            ingredients: [],
-            instructions: '',
-            match: 0.0,
+            recipeTitles: [],
+            recipeIngredients: [],
+            recipeInstructions: [],
         };
     }
 
     componentDidMount() {
-        // URL should provide only one response: Epi's 50-Ingredient Super Bowl Nachos
+        /*
+        TO-DO:
+        - Input checking: don't allow invalid queries
+        */
         const apiURL = `https://mint-green-recipes.herokuapp.com/recipes/?ingredients=${
             removeSpaces(this.state.query)}`;
-        // fetch('https://mint-green-recipes.herokuapp.com/recipes/?ingredients=tomatoes,bell%20pepper,potatoes,salt,cream,chicken,bacon,garlic')
-        // fetch('http://jsonplaceholder.typicode.com/users/1')
         fetch(apiURL)
             .then(result => result.json())
             .then((data) => {
+                const tmpTitles = [];
+                const tmpIngredients = [];
+                const tmpInstructions = [];
+
+                for (let i = 0; i < Object.keys(data).length; i += 1) {
+                    tmpTitles.push(data[i].title);
+                    tmpIngredients.push(data[i].ingredients);
+                    tmpInstructions.push(data[i].instructions);
+                }
+
                 this.setState({
                     isLoaded: true,
-                    resp: JSON.stringify(data[0]),
-                    title: data[0].title,
-                    ingredients: data[0].ingredients,
-                    instructions: data[0].instructions,
-                    match: data[0].match,
-                    // id: data.id,
-                    // title: data.title,
-                    // completed: data.completed,
+                    recipeTitles: tmpTitles,
+                    recipeIngredients: tmpIngredients,
+                    recipeInstructions: tmpInstructions,
                 });
             },
           // Note: it's important to handle errors here
@@ -60,47 +64,30 @@ export default class Homepage extends Component {
     }
 
     render() {
-        // First, check if anything has been entered
-        // Then render based on presence of input
-        // TODO: input checking should probably go here
-        const currentQuery = this.state.query;
-        // const respone = this.state.resp[1].id;
-        // const resultsLoaded = this.state.isLoaded;
         let queryInfo;
-        if (currentQuery) {
-            // User gave a query; check it, call API, etc.
-            // When Results component exists, this could be: queryInfo = <Results />
+        const isLoaded = this.state.isLoaded;
+
+        // Data from API
+        const recipeTitles = this.state.recipeTitles;
+        const recipeIngredients = this.state.recipeIngredients;
+        const recipeInstructions = this.state.recipeInstructions;
+
+        if (isLoaded) {
             queryInfo = (
                 <div>
-                    <h2>Cleaned search: {removeSpaces(currentQuery)}</h2>
-                    {/* <h2>Cleaned search: {currentQuery.trim()}</h2> */}
-                    {/* <h2>The type of the query: {typeof currentQuery}</h2>
-                    <h2>Length of query provided: {currentQuery.length}</h2>
-                    <h2>Is loaded?: {String(this.state.isLoaded)}</h2> */}
-                    <h2>Results from call:</h2>
-                    <div>
-                        {/* Username: {String(this.state.resp.data.username)} <br /> */}
-                        {/* JSON: <br /> {this.state.resp} <br /> */}
-                        Title: <br /> {this.state.title} <br />
-                        Ingredients: <br /> {this.state.ingredients.join(', ')} <br />
-                        Match: <br /> {this.state.match} <br /><br />
-                        Instructions: <br /> {this.state.instructions} <br />
-                        {/* List JSON Data has to be accessed via .map() */}
-                        {/* Map: {this.state.resp.map(recipeData =>
-                            recipeData.title)
-                        } */}
-                        {/* Response obj: {respone} */}
-                        {/* userId: {String(this.state.userId)} <br />
-                        id: {String(this.state.id)} <br />
-                        title: {String(this.state.title)} <br />
-                        completed: {String(this.state.completed)} */}
-                    </div>
-                </div>
-            );
+                    {/* All objects will have the same number of entries, so single index is OK */}
+                    {Object.keys(recipeTitles).map((t, i) =>
+                        (<p key={recipeTitles[t]}>
+                            Recipe {i}: {recipeTitles[t]}<br />
+                            Ingredients: {recipeIngredients[t].join(', ')}<br />
+                            Instructions: {recipeInstructions[t]}<br />
+                            <br /><br />
+                        </p>))}
+                </div>);
         } else {
             queryInfo = (
                 // User didn't provide a query yet/on default home page
-                <div>
+                <div key="No-response">
                     <h2>Go ahead and provide a search for me</h2>
                 </div>
             );
