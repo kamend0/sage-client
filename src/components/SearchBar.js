7aad7d8f1379d469af456e97b21cbd9e3716ddf5
kamend0 <mendoza.kollin@gmail.com>
2022-07-05 12:20:14 -0700

    Convert SearchBar to functional component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-/*
-TO-DO: Convert to function, since we now know that we don't
-    need state.
-*/
-// eslint-disable-next-line react/prefer-stateless-function
-export default class SearchBar extends Component {
-    render() {
-        return (
-            <form action="/" method="get" id="search-bar">
-                <input
-                    type="text"
-                    id="search-bar-box"
-                    placeholder="Enter your ingredients separated by commas..."
-                    name="q" />
-                <button type="submit" id="search-bar-submit">Search</button>
-            </form>
-        );
-    }
-}
+
+const SearchBar = () => (
+    <form action="/" method="get" id="search-bar">
+        <input
+            type="text"
+            id="search-bar-box"
+            placeholder="Enter your ingredients separated by commas..."
+            name="q" />
+        <button type="submit" id="search-bar-submit">Search</button>
+    </form>
+    );
+
+export default SearchBar;
